4382dff8d4fb58c383847db1f88ae84abeabe214
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-06 17:14:41 -0500

    agregando todos al state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoModule } from './todos/todo.module';
-import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoModule } from "./todos/todo.module";
+import { FooterComponent } from "./footer/footer.component";
+
+import { StoreModule } from "@ngrx/store";
+import { todoReducer } from "./todos/todo.reducer";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
+
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent
-  ],
+  declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TodoModule
+    TodoModule,
+    ReactiveFormsModule,
+    StoreModule.forRoot({ todos: todoReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -2,13 +2,13 @@ import { createReducer, on } from "@ngrx/store";
 import { crear } from "./todo.action";
 import { Todo } from "../models/todo";
 
-export const initialState: Todo[] = [];
+export const initialState: Todo[] = [new Todo("Salvar al mundo")];
 
 const _todoReducer = createReducer(
   initialState,
   on(crear, (state, { texto }) => [...state, new Todo(texto)])
 );
 
-export function counterReducer(state, action) {
+export function todoReducer(state, action) {
   return _todoReducer(state, action);
 }
