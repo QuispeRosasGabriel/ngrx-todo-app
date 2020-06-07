bb85e881794426651a9c4ba8949f1842440f0e1f
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-06-06 21:22:15 -0500

    blur

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, ViewChild, ElementRef } from "@angular/core";
 import { Todo } from "src/app/models/todo";
-import { FormControl } from "@angular/forms";
+import { FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-todo-item",
@@ -9,10 +9,29 @@ import { FormControl } from "@angular/forms";
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @ViewChild("inputFisico", { static: true })
+  txtInputFisico: ElementRef;
 
   chkCompletado: FormControl;
+  txtInput: FormControl;
+  editando: boolean = false;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.chkCompletado = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+    console.log(this.txtInputFisico);
+  }
+
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1000);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+  }
 }
